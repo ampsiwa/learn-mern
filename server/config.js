f83c720855afef04e5bc2fd131a5ab584425a70c
ampsiwa <ampere_555@hotmail.com>
2016-03-27 13:08:15 +0700

    Edit ip server

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://52.10.203.129:27017/mern',
   port: process.env.PORT || 8000,
 };
 
